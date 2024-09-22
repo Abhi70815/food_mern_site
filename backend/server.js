c46a3fcbe80b4d9f9a5b21cbccb7aad4ce1e1582
jeetungaa <vaasumarothia45@gmail.com>
2024-09-22 17:30:00 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,11 @@ import foodRouter from "./routes/foodRoute.js";
 import userRouter from "./routes/userRoute.js";
 import cartRouter from "./routes/cartRoute.js";
 import orderRouter from "./routes/orderRoute.js";
+import path from 'path';
+import 'dotenv/config'
+import { fileURLToPath } from 'url';
+
+
 
 // app config
 const app = express();
@@ -13,6 +18,9 @@ const PORT = 4000;
 //middleware
 app.use(express.json());
 app.use(cors());
+
+
+
 
 // db connection
 connectDB()
@@ -24,17 +32,28 @@ app.use("/api/user", userRouter)
 app.use("/api/cart", cartRouter)
 app.use("/api/order", orderRouter)
 
-if (process.env.NODE_ENV === 'production') {
-    //*Set static folder up in production
-    app.use(express.static('client/build'));
-
-    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
-  }
 
 
-app.get("/", (req, res) => {
-    res.send("API working")
-})
+
+//deploymentcodee
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const resolvedPath = path.resolve(__dirname);
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(resolvedPath, '../frontend/dist')))
+
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(resolvedPath, "frontend", "dist", "index.html"))
+    })
+}else{
+    app.get("/", (req, res) => {
+        res.send("API working")
+    })
+    
+}
+
+
 
 app.listen(PORT, () => {
     console.log(`Server started`)
